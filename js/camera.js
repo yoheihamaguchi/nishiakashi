ea811b2f49481ea2bf0371a7a8b85590eed49d36
yoheihamaguchi <yoheihamaguchi251@gmail.com>
2021-01-15 16:57:39 +0900

    写真機能完成

--- js/camera.js
+++ js/camera.js
@@ -1,68 +1,71 @@
-const video = document.querySelector("#camera");
-const canvas = document.querySelector("#picture");
-const shutter = document.querySelector("#shutter");
-const a = document.querySelector("#download")
-
-/** カメラ設定 */
-const constraints = {
-    audio: false,
-    video: {
-        width: { ideal: 1080 },
-        height: { ideal: 1920 },
-        // フロントカメラを利用する
-        facingMode: "user"
-    }
-};
-
-/**
- * カメラ起動
- */
-const getLocalMediaStream = (mediaStream) => {
-    // const mediaRecorder = new MediaRecorder(mediaStream);
-    // console.log(mediaRecorder)
-
-    video.srcObject = mediaStream;
-    video.onloadedmetadata = (e) => {
-        video.play();
-    };
-}
-
-/**
- * カメラ映像取得エラー
- */
-const handleCameraError = (err) => {
-    console.log(err.name + ": " + err.message);
-}
-
-/**
- * カメラを<video>と同期
- */
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    console.log('success')
-    navigator.mediaDevices.getUserMedia(constraints)
-        .then(getLocalMediaStream)
-        .catch(handleCameraError);
-    console.log()
-} else {
-    console.log("not supported.")
-}
-
-/**
- * シャッターボタン（写真撮影時）
- */
-shutter.addEventListener("click", () => {
+window.onload = () => {
+    const video = document.querySelector("#camera");
+    const canvas = document.querySelector("#picture");
+    const shutter = document.querySelector("#shutter");
+    const a = document.querySelector("#download")
     const ctx = canvas.getContext("2d");
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    canvas.toBlob((blob) => {
-        console.log(blob)
-        let newImg = document.createElement("img")
-        a.href = URL.createObjectURL(blob)
-        console.log('url:', a.href)
+    /** カメラ設定 */
+    const constraints = {
+        audio: false,
+        video: {
+            width: { ideal: 1080 },
+            height: { ideal: 1920 },
+            // フロントカメラを利用する
+            facingMode: "user"
+        }
+    };
 
-        newImg.src = a.href
-        document.body.appendChild(newImg)
-    }, "image/png")
+    /**
+     * カメラ起動
+     */
+    const getLocalMediaStream = (mediaStream) => {
+        // const mediaRecorder = new MediaRecorder(mediaStream);
+        // console.log(mediaRecorder)
+        video.srcObject = mediaStream;
+        video.play();
+    }
 
-    URL.revokeObjectURL(a.href)
-});
+    /**
+     * カメラ映像取得エラー
+     */
+    const handleCameraError = (err) => {
+        console.log(err.name + ": " + err.message);
+    }
+
+    /**
+     * カメラを<video>と同期
+     */
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        console.log('success')
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(getLocalMediaStream)
+            .catch(handleCameraError);
+        console.log()
+    } else {
+        console.log("not supported.")
+    }
+
+    /**
+     * シャッターボタン（写真撮影時）
+     */
+    shutter.addEventListener("click", () => {
+        video.pause()
+        setTimeout(() => {
+            video.play();
+        }, 500);
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        canvas.toBlob((blob) => {
+            console.log(blob)
+            a.href = URL.createObjectURL(blob)
+            // console.log('url:', a.href)
+
+            // let newImg = document.createElement("img")
+            // newImg.src = a.href
+            // document.body.appendChild(newImg)
+        }, "image/png")
+
+        URL.revokeObjectURL(a.href)
+    });
+}
