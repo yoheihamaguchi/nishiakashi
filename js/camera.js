535b0714d759bbae486f16002b4be432806c34ca
yoheihamaguchi <yoheihamaguchi251@gmail.com>
2021-01-19 11:45:19 +0900

    一旦提出用

--- js/camera.js
+++ js/camera.js
@@ -4,8 +4,11 @@ window.onload = () => {
     const shutter = document.querySelector("#shutter");
     const a = document.querySelector("#download")
     const ctx = canvas.getContext("2d");
+    const img = document.querySelector("img")
     video.setAttribute("style", "margin-bottom:40px")
     a.style.display = "none"
+    canvas.style.display = "none"
+    shutter.style.display = "none"
 
     /** カメラ設定 */
     const constraints = {
@@ -22,8 +25,6 @@ window.onload = () => {
      * カメラ起動
      */
     const getLocalMediaStream = (mediaStream) => {
-        // const mediaRecorder = new MediaRecorder(mediaStream);
-        // console.log(mediaRecorder)
         video.srcObject = mediaStream;
         video.play();
     }
@@ -56,7 +57,19 @@ window.onload = () => {
         setTimeout(() => {
             video.play();
         }, 500);
-        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        let images = [video, img]
+        images.forEach((image, index) => {
+            ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+            console.log('index:', index)
+
+        })
+        // for (i in images) {
+        //     ctx.drawImage(images[i], 0, 0, canvas.width, canvas.height);
+        //     console.log(canvas.width)
+        //     images[i].width = images[i].width / 2
+        //     images[i].height = images[i].height / 2
+        // }
 
         canvas.toBlob((blob) => {
             console.log(blob)
