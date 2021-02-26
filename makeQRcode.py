5e27633ba18ed2e8f24926a386aff9bdf7293db3
yoheihamaguchi <yoheihamaguchi251@gmail.com>
2021-02-26 17:34:54 +0900

    一旦完成

--- makeQRcode.py
+++ makeQRcode.py
@@ -1,19 +1,10 @@
 import qrcode, os
 
-# def makeQRcode():
-file_name = "qr_code.png"
-print("QRコードに変換したい文字列を入力してください: ", end="")
+def makeQRcode(qr_string):
+    file_name = "picture.png"
 
-# キーボードから変換したい文字列を入力させる
-qr_string = input()
+    # QRコード画像データ生成
+    img = qrcode.make(qr_string)
 
-# QRコード画像データ生成
-img = qrcode.make(qr_string)
-
-# 画像ファイルとして保存
-img.save(file_name)
-
-# 現在のディレクトリ位置を取得
-current_dir = os.getcwd()
-
-print("「{0}\\{1}」にQRコード画像を保存しました".format(current_dir, file_name))
+    # 画像ファイルとして保存
+    img.save(file_name)
