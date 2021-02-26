a046ced7d59d6ddc89b015ad93e95c78ac27b340
yoheihamaguchi <yoheihamaguchi251@gmail.com>
2021-02-26 11:14:39 +0900

    ファイル作成検知後、Driveアップロード

--- watch.py
+++ watch.py
@@ -10,25 +10,32 @@ import time
 import os
 import hashlib
 import pathlib
+import pprint
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 
+# 認証情報を生成
+gauth = GoogleAuth()
+
+# Creates local webserver and auto handles authentication.
+gauth.LocalWebserverAuth() 
+
+# 認証情報をもとにGoogleDriveFileオブジェクトを生成
+drive = GoogleDrive(gauth)
 
 class ChangeHandler(FileSystemEventHandler):
-    # def __init__(self):
-    #     with open(FILENAME, 'rb') as f:
-    #         # 最後のイベント時でのファイルの md5 ハッシュ値を入れておく
-    #         self.oldmd5 = hashlib.md5(f.read()).hexdigest()
-    #         # 最後のイベント時でのファイルの pos 値を入れておく
-    #         self.pos = f.tell()
-    def on_deleted(self, event):
-        """
-        ファイル削除検知
-        :param event:
-        :return:
-        """
-        src_name = os.path.basename(event.src_path)
-        print(f'{src_name}を削除しました')
+    def __init__(self,path):
+        self.path = path
+    # def on_deleted(self, event):
+    #     """
+    #     ファイル削除検知
+    #     :param event:
+    #     :return:
+    #     """
+    #     src_name = os.path.basename(event.src_path)
+    #     print(f'{src_name}を削除しました')
 
     def on_created(self, event):
         """
@@ -38,8 +45,22 @@ class ChangeHandler(FileSystemEventHandler):
         """
         # ファイル名取得
         src_name = os.path.basename(event.src_path)
-        print(f'{src_name}ができました')
+        
+        # パスの確認のため取得　最終的に削除する部分
+        src_path = pathlib.Path(self.path) / pathlib.Path(f'{src_name}')
 
+        # パスの確認のため取得　最終的に削除する部分
+        print(f'{src_name}ができました')
+        print(f'src_path: {src_path}')
+
+        f = drive.CreateFile()
+        f.SetContentFile(src_name)
+        f.Upload()
+
+        # パスの確認のため取得　最終的に削除する部分
+        print('upload: ',f)
+
+    
 if __name__ == '__main__':
     # ディレクトリの取得と，カレントディレクトリの変更
     if len(sys.argv) > 1:
@@ -47,8 +68,9 @@ if __name__ == '__main__':
         os.chdir(path)
     else:
         path = os.path.abspath(os.path.dirname(__file__))
+        print("dirname: ",path)
 
-    event_handler = ChangeHandler()
+    event_handler = ChangeHandler(path)
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
